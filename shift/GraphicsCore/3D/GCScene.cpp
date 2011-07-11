48ce0747480282b486fc72ff7c91a359b1aea8a5
Jorj Pimm <jorjpimm@gmail.com>
2011-07-11 13:53:17 +0100

    stuff

--- shift/GraphicsCore/3D/GCScene.cpp
+++ shift/GraphicsCore/3D/GCScene.cpp
@@ -25,10 +25,10 @@ void GCScene::render(XRenderer *r) const
   {
   r->setProjectionTransform(cameraProjection());
 
-    glLoadIdentity();						// Reset The View
+    //glLoadIdentity();						// Reset The View
 
 
-    glTranslatef(-1.5f,0.0f,-6.0f);					// Move Left 1.5 Units And Into The Screen 6.0
+    /*glTranslatef(-1.5f,0.0f,-6.0f);					// Move Left 1.5 Units And Into The Screen 6.0
 
 
     glBegin(GL_TRIANGLES);						// Drawing Using Triangles
@@ -44,12 +44,27 @@ void GCScene::render(XRenderer *r) const
       glVertex3f( 1.0f, 1.0f, 0.0f);				// Top Right
       glVertex3f( 1.0f,-1.0f, 0.0f);				// Bottom Right
       glVertex3f(-1.0f,-1.0f, 0.0f);				// Bottom Left
-    glEnd();
+    glEnd();*/
 
   XTransform t = XTransformUtilities::lookAt(XVector3D(10,5,10), XVector3D::Zero(), XVector3D(0,1,0));
 
   glLoadIdentity();						// Reset The View
   r->pushTransform(t);
+
+  glBegin(GL_TRIANGLES);						// Drawing Using Triangles
+    glVertex3f( 0.0f, 1.0f, 0.0f);				// Top
+    glVertex3f(-1.0f,-1.0f, 0.0f);				// Bottom Left
+    glVertex3f( 1.0f,-1.0f, 0.0f);				// Bottom Right
+  glEnd();							// Finished Drawing The Triangle
+
+  glTranslatef(3.0f,0.0f,0.0f);					// Move Right 3 Units
+
+  glBegin(GL_QUADS);						// Draw A Quad
+    glVertex3f(-1.0f, 1.0f, 0.0f);				// Top Left
+    glVertex3f( 1.0f, 1.0f, 0.0f);				// Top Right
+    glVertex3f( 1.0f,-1.0f, 0.0f);				// Bottom Right
+    glVertex3f(-1.0f,-1.0f, 0.0f);				// Bottom Left
+  glEnd();
 
   XGeometry x;
   XModeller m(&x, 128);
