ed3a16f0bdadb7800b49796e8148960b28eae319
Jorj Pimm <jorjpimm@gmail.com>
2011-08-02 21:24:12 +0100

    work on shader classes

--- shift/GraphicsCore/3D/GCShader.cpp
+++ shift/GraphicsCore/3D/GCShader.cpp
@@ -1,11 +1,44 @@
 #include "GCShader.h"
 #include "XRenderer.h"
 
+S_IMPLEMENT_PROPERTY(GCShaderComponent)
+
+SPropertyInformation *GCShaderComponent::createTypeInformation()
+  {
+  SPropertyInformation *info = SPropertyInformation::create<GCShaderComponent>("GCShaderComponent");
+
+  info->add(&GCShaderComponent::source, "source");
+
+  return info;
+  }
+
+GCShaderComponent::GCShaderComponent()
+  {
+  }
+
+S_IMPLEMENT_PROPERTY(GCFragmentShaderComponentVShader)
+
+SPropertyInformation *GCFragmentShaderComponentVShader::createTypeInformation()
+  {
+  SPropertyInformation *info = SPropertyInformation::create<GCFragmentShaderComponentVShader>("GCFragmentShaderComponentVShader");
+  return info;
+  }
+
+S_IMPLEMENT_PROPERTY(GCVertexShaderComponent)
+
+SPropertyInformation *GCVertexShaderComponent::createTypeInformation()
+  {
+  SPropertyInformation *info = SPropertyInformation::create<GCVertexShaderComponent>("GCVertexShaderComponent");
+  return info;
+  }
+
 S_IMPLEMENT_PROPERTY(GCShader)
 
 SPropertyInformation *GCShader::createTypeInformation()
   {
   SPropertyInformation *info = SPropertyInformation::create<GCShader>("GCShader");
+
+  info->add(&GCShader::components, "components");
 
   return info;
   }
--- shift/GraphicsCore/3D/GCShader.h
+++ shift/GraphicsCore/3D/GCShader.h
@@ -3,8 +3,32 @@
 
 #include "GCGlobal.h"
 #include "sentity.h"
+#include "sbaseproperties.h"
 #include "sbasepointerproperties.h"
 #include "XShader.h"
+
+class GRAPHICSCORE_EXPORT GCShaderComponent : public SEntity
+  {
+  S_ENTITY(GCShaderComponent, SEntity, 0)
+public:
+  GCShaderComponent();
+
+  StringProperty source;
+  };
+
+class GRAPHICSCORE_EXPORT GCFragmentShaderComponent : public GCShaderComponent
+  {
+  S_ENTITY(GCFragmentShaderComponent, GCShaderComponent, 0)
+  };
+
+class GRAPHICSCORE_EXPORT GCVertexShaderComponent : public GCShaderComponent
+  {
+  S_ENTITY(GCVertexShaderComponent, GCShaderComponent, 0)
+  };
+
+S_TYPED_POINTER_TYPE(GCShaderComponentPointer, GCShaderComponent);
+
+S_TYPED_POINTER_ARRAY_TYPE(GCShaderComponentPointerArray, GCShaderComponentPointer);
 
 class GRAPHICSCORE_EXPORT GCShader : public SEntity
   {
@@ -13,6 +37,8 @@ public:
   GCShader();
 
   void bind(XRenderer *r) const;
+
+  GCShaderComponentPointerArray components;
 
 private:
   XShader shader;
