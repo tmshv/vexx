ab81a6a2fc9fba68b88d17d5945693bcfc761db7
Jorj Pimm <jorjpimm@gmail.com>
2012-02-03 21:36:30 +0000

    convolve working

--- EksCore/testProject/main.cpp
+++ EksCore/testProject/main.cpp
@@ -39,7 +39,7 @@ int main()
   add.add(arr, arr2);
 
   XMatrix3x3 centeringOffset = XMatrix3x3::Identity();
-  tr1.col(2) = XVector3D(-1, -1, 1);
+  centeringOffset.col(2) = XVector3D(-1, -1, 1);
 
   XMathsOperation convolve;
   float convData[] = { 0.05f, 0.25f, 0.05f,
@@ -60,68 +60,43 @@ int main()
   XMathsOperation mul;
   mul.multiply(shuffle, arr);*/
 
-  XMathsResult res(convolve);
+  {
+  XMathsResult res(add);
   float* data = (float*)res.data();
   xsize w = res.dataWidth();
   xsize h = res.dataHeight();
   xsize channels = res.dataChannels();
-
-  QString out;
+  
+  QString out = "\n";
   for(xsize y=0; y < h; ++y)
     {
     for(xsize x=0; x < w; ++x)
       {
-      out += QString::number(data[x*channels + y*w*channels]) + " ";
+      out += QString::number(data[x*channels + y*w*channels]).leftJustified(2) + " ";
       }
 
     out += '\n';
     }
   qDebug() << out;
   }
-
-/*
-int main( )
+  
   {
-  Eigen::VectorXf a(3);
-  Eigen::VectorXf b(a);
-  Eigen::VectorXf c(b);
+  XMathsResult res(convolve);
+  float* data = (float*)res.data();
+  xsize w = res.dataWidth();
+  xsize h = res.dataHeight();
+  xsize channels = res.dataChannels();
 
-  a = Eigen::Vector4f(1, 2, 3, 4);
-  b = Eigen::Vector4f(3, 2, 1, 0);
+  QString out = "\n";
+  for(xsize y=0; y < h; ++y)
+    {
+    for(xsize x=0; x < w; ++x)
+      {
+      out += QString::number(data[x*channels + y*w*channels]).leftJustified(2) + " ";
+      }
 
-  c = a + b;
-
-  Eigen::Matrix<float,Eigen::Dynamic,Eigen::Dynamic> x(1,3);
-  Eigen::Matrix<float,Eigen::Dynamic,Eigen::Dynamic> y(3,1);
-
-  x = y;
-
-  x.resize(4,4);
-  x.resize(2,3);
-
-  y = x;
-
-  y = a;
-
-  Eigen::Vector3f d = a.block<3, 1>(0, 0);
-
-  c = a;
-
-  Eigen::Vector3f e = a.block<3, 1>(0, 0);
-
-  Eigen::ArrayXf ddd = c;
-
-  ddd = a;
-
-  Eigen::Matrix3f f;
-
-  f.row(0) = e;
-
-  Eigen::Affine3f m;
-
-
-  m.matrix().row(0).head<3>() = e;
-
-  return EXIT_SUCCESS;
+    out += '\n';
+    }
+  qDebug() << out;
   }
-*/
+  }
