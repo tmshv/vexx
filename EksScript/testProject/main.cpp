c70bea03d157a1163e3b2ef083b643255638afaa
Jorj Pimm <jorjpimm@gmail.com>
2012-02-17 12:50:06 +0000

    tests

--- EksScript/testProject/main.cpp
+++ EksScript/testProject/main.cpp
@@ -2,20 +2,75 @@
 
 using namespace v8;
 
+class SomeClass
+{
+public:
+  static int getA()
+  {
+    return a;
+  }
+
+  static void setA(const int &inA)
+  {
+    a = inA;
+  }
+
+  static int a;
+};
+
+struct WrappedStaticProperty
+{
+  Handle<Value> (*get)(const WrappedStaticProperty *);
+  void (*set)(const WrappedStaticProperty *, const Local<Value>& val);
+  T (*getT)();
+  void (*setT)(const T& val);
+
+  template <typename T>
+  static WrappedStaticProperty wrap(T get(), void set(const T&))
+  {
+    WrappedStaticProperty a = { getHelper<T>, setHelper<T>, get, set };
+
+    return a;
+  }
+
+  static Handle<Value> Getter(Local<String>, const AccessorInfo& info)
+  {
+    Local<Value> v = info.Data();
+    WrappedStaticProperty *p = (WrappedStaticProperty *)External::Unwrap(v);
+    return p->get(p);
+  }
+
+  static void Setter(Local<String>, Local<Value> value, const AccessorInfo& info)
+  {
+    Local<Value> v = info.Data();
+    WrappedStaticProperty *p = (WrappedStaticProperty *)External::Unwrap(v);
+    return p->set(p, value);
+  }
+
+};
+
+
 int main(int argc, char* argv[]) {
+
+  Handle<ObjectTemplate> global_templ = ObjectTemplate::New();
+
+  WrappedStaticProperty stat = WrappedStaticProperty::wrap(SomeClass::getA, SomeClass::setA);
+
+  Handle<Value> data = External::Wrap(&stat);
+  global_templ->SetAccessor(String::New("a"), WrappedStaticProperty::Getter, WrappedStaticProperty::Setter, data);
 
   // Create a stack-allocated handle scope.
   HandleScope handle_scope;
 
   // Create a new context.
-  Persistent<Context> context = Context::New();
+  Persistent<Context> context = Context::New(NULL, global_templ);
 
   // Enter the created context for compiling and
   // running the hello world script.
   Context::Scope context_scope(context);
 
   // Create a string containing the JavaScript source code.
-  Handle<String> source = String::New("'Hello' + ', World!'");
+  Handle<String> source = String::New("'Hello' + a.toString()");
 
   // Compile the source code.
   Handle<Script> script = Script::Compile(source);
