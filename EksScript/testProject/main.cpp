1b7ad74a63ba63f47d08d21bcb1fed256e3de3ec
Jorj Pimm <jorjpimm@gmail.com>
2012-02-20 12:18:35 +0000

    work

--- EksScript/testProject/main.cpp
+++ EksScript/testProject/main.cpp
@@ -1,67 +1,45 @@
 #include "v8.h"
 #include "QMetaType"
+#include "QVariant"
 #include "QHash"
-
-using namespace v8;
 
 #ifndef Q_CC_MSVC
 # error not setup for non msvc, needs solid testing in member pointers
 #endif
 
-class SomeClass
+template <typename T> v8::Handle<v8::Value> Pack(T t);
+template <typename T> T Unpack(v8::Handle<v8::Value>);
+
+template <> v8::Handle<v8::Value> Pack(int i)
 {
-public:
-  virtual ~SomeClass() {}
-
-  virtual int getNonStatic()
-  {
-    return nonStatic;
-  }
-
-  virtual void setNonStatic(const int &inA)
-  {
-    nonStatic = inA;
-  }
-
-  static int getA()
-  {
-    return a;
-  }
-
-  static void setA(const int &inA)
-  {
-    a = inA;
-  }
-
-  int nonStatic;
-  static int a;
-};
-
-int SomeClass::a = 0;
-
-template <typename T> Handle<Value> Pack(T t);
-template <typename T> T Unpack(Handle<Value>);
-
-template <> Handle<Value> Pack(int i)
-{
-  return Integer::New(i);
+  return v8::Integer::New(i);
 }
 
-template <> int Unpack(Handle<Value> v)
+template <> int Unpack(v8::Handle<v8::Value> v)
 {
   return v->Int32Value();
 }
 
-struct WrappedStaticProperty
+class WrappedMember
+{
+
+};
+
+struct WrappedPropertyMap : public WrappedMember
+{
+
+};
+
+struct WrappedStaticProperty : public WrappedMember
 {
   void *get;
   void *set;
 
   template <typename T>
-  static Handle<Value> StaticGetter(Local<String>, const AccessorInfo& info)
+  static v8::Handle<v8::Value> StaticGetter(v8::Local<v8::String>, const v8::AccessorInfo& info)
   {
-    Local<Value> v = info.Data();
-    WrappedStaticProperty *p = (WrappedStaticProperty *)External::Unwrap(v);
+    v8::Local<v8::Value> v = info.Data();
+    WrappedStaticProperty *p = (WrappedStaticProperty *)v8::External::Unwrap(v);
 
     typedef T (*GET)() ;
     GET g = (GET)p->get;
@@ -72,10 +50,10 @@ struct WrappedStaticProperty
   }
 
   template <typename T>
-  static void StaticSetter(Local<String>, Local<Value> value, const AccessorInfo& info)
+  static void StaticSetter(v8::Local<v8::String>, v8::Local<v8::Value> value, const v8::AccessorInfo& info)
   {
-    Local<Value> v = info.Data();
-    WrappedStaticProperty *p = (WrappedStaticProperty *)External::Unwrap(v);
+    v8::Local<v8::Value> v = info.Data();
+    WrappedStaticProperty *p = (WrappedStaticProperty *)v8::External::Unwrap(v);
 
     typedef void (*SET)(const T &);
     SET g = (SET)p->set;
@@ -84,23 +62,23 @@ struct WrappedStaticProperty
   }
 };
 
-struct WrappedProperty
+struct WrappedProperty : public WrappedMember
 {
   void *get[2];
   void *set[2];
 
   template <typename T>
-  static Handle<Value> Getter(Local<String>, const AccessorInfo& info)
+  static v8::Handle<v8::Value> Getter(v8::Local<v8::String>, const v8::AccessorInfo& info)
   {
     class CLASS
     {
     };
 
-    Local<Value> v = info.Data();
-    WrappedProperty *p = (WrappedProperty *)External::Unwrap(v);
+    v8::Local<v8::Value> v = info.Data();
+    WrappedProperty *p = (WrappedProperty *)v8::External::Unwrap(v);
 
-    Local<Object> self = info.Holder();
-    Local<External> wrap = Local<External>::Cast(self->GetInternalField(0));
+    v8::Local<v8::Object> self = info.Holder();
+    v8::Local<v8::External> wrap = v8::Local<v8::External>::Cast(self->GetInternalField(0));
     void* ptr = wrap->Value();
 
     typedef T (CLASS::*GET)();
@@ -111,17 +89,17 @@ struct WrappedProperty
   }
 
   template <typename T>
-  static void Setter(Local<String>, Local<Value> value, const AccessorInfo& info)
+  static void Setter(v8::Local<v8::String>, v8::Local<v8::Value> value, const v8::AccessorInfo& info)
   {
     class CLASS
     {
     };
 
-    Local<Value> v = info.Data();
-    WrappedProperty *p = (WrappedProperty *)External::Unwrap(v);
+    v8::Local<v8::Value> v = info.Data();
+    WrappedProperty *p = (WrappedProperty *)v8::External::Unwrap(v);
 
-    Local<Object> self = info.Holder();
-    Local<External> wrap = Local<External>::Cast(self->GetInternalField(0));
+    v8::Local<v8::Object> self = info.Holder();
+    v8::Local<v8::External> wrap = v8::Local<v8::External>::Cast(self->GetInternalField(0));
     void* ptr = wrap->Value();
 
     typedef void (CLASS::*SET)(const T &);
@@ -159,93 +137,248 @@ public:
 };
 QHash<int, TypeWrapper> TypeWrapper::_wrappedTypes;
 
-void addStaticAccessor(Handle<ObjectTemplate>& tmpl, const char *name, int id, void *get, void *set)
-{
-  WrappedStaticProperty* wrappedData = new WrappedStaticProperty;
-  wrappedData->get = get;
-  wrappedData->set = set;
-
-  Handle<Value> data = External::Wrap(wrappedData);
-
-  const TypeWrapper wrap = TypeWrapper::findType(id);
-  tmpl->SetAccessor(String::New(name), (v8::AccessorGetter)wrap.staticGet, (v8::AccessorSetter)wrap.staticSet, data);
-}
-
-template <typename GETFUNC, typename SETFUNC>
-void addAccessor(Handle<ObjectTemplate>& tmpl, const char *name, int id, GETFUNC g,  SETFUNC s)
-{
-  _ASSERT(sizeof(g) <= (sizeof(void *)*2));
-  _ASSERT(sizeof(s) <= (sizeof(void *)*2));
-
-  WrappedProperty* wrappedData = new WrappedProperty;
-  memcpy(wrappedData->get, &g, sizeof(void*)*2);
-  memcpy(wrappedData->set, &s, sizeof(void*)*2);
-
-  Handle<Value> data = External::Wrap(wrappedData);
-
-  const TypeWrapper wrap = TypeWrapper::findType(id);
-  tmpl->SetAccessor(String::New(name), (v8::AccessorGetter)wrap.get, (v8::AccessorSetter)wrap.set, data);
-}
-
 class Interface
 {
+public: // external
+  Interface() : _template(v8::ObjectTemplate::New())
+  {
+    _template->SetInternalFieldCount(1);
+  }
 
+  template <typename T, typename GETFUNC, typename SETFUNC>
+  void addProperty(const char *name, GETFUNC g,  SETFUNC s)
+  {
+    _ASSERT(sizeof(g) <= (sizeof(void *)*2));
+    _ASSERT(sizeof(s) <= (sizeof(void *)*2));
+
+    void *get[2];
+    void *set[2];
+    memcpy(get, &g, sizeof(void*)*2);
+    memcpy(set, &s, sizeof(void*)*2);
+
+    addProperty(name, qMetaTypeId<T>(), get, set);
+  }
+
+  void addPropertyMap(WrappedPropertyMap::Getter g, WrappedPropertyMap::Setter s=0, WrappedPropertyMap::StaticGetter sG=0, , WrappedPropertyMap::StaticGetter sS=0)
+  {
+    WrappedPropertyMap* wrappedData = new WrappedPropertyMap;
+    wrappedData->get = g;
+    wrappedData->set = s;
+    wrappedData->staticGet = sG;
+    wrappedData->staticSet = sS;
+    addMember(wrappedData);
+
+    v8::Handle<v8::Value> data = v8::External::Wrap(wrappedData);
+
+    _template->SetNamedPropertyHandler(WrappedPropertyMap::GetHelper, WrappedPropertyMap::SetHelper, 0, 0, 0, data);
+  }
+
+  template <typename T>
+  void addStaticProperty(const char *name, void *get, void *set)
+  {
+    addStaticProperty(name, qMetaTypeId<T>(), get, set);
+  }
+
+private: // internal
+  void addStaticProperty(const char *name, int id, void *get, void *set)
+  {
+    WrappedStaticProperty* wrappedData = new WrappedStaticProperty;
+    wrappedData->get = get;
+    wrappedData->set = set;
+
+    addMember(wrappedData);
+
+    v8::Handle<v8::Value> data = v8::External::Wrap(wrappedData);
+
+    const TypeWrapper wrap = TypeWrapper::findType(id);
+    _template->SetAccessor(v8::String::New(name), (v8::AccessorGetter)wrap.staticGet, (v8::AccessorSetter)wrap.staticSet, data);
+  }
+
+  void addProperty(const char *name, int id, void *get[2], void *set[2])
+  {
+
+    WrappedProperty* wrapped = new WrappedProperty;
+    memcpy(wrapped->get, get, sizeof(get));
+    memcpy(wrapped->set, set, sizeof(set));
+
+    addMember(wrapped);
+    v8::Handle<v8::Value> data = v8::External::Wrap(wrapped);
+
+    const TypeWrapper wrap = TypeWrapper::findType(id);
+    _template->SetAccessor(v8::String::New(name), (v8::AccessorGetter)wrap.get, (v8::AccessorSetter)wrap.set, data);
+  }
+
+private:
+  void addMember(WrappedMember* )
+    {
+    // keep list and delete them later...
+    }
+
+  v8::Local<v8::ObjectTemplate> _template;
+  friend class Object;
 };
+
+class Object
+  {
+public:
+  Object(const Interface* ifc, void *thisPtr)
+    : _interface(ifc),
+    _object(_interface->_template->NewInstance())
+  {
+    _object->SetInternalField(0, v8::External::New(thisPtr));
+  }
+
+private:
+  const Interface *_interface;
+  v8::Local<v8::Object> _object;
+  friend class Context;
+  };
+
+class Engine
+  {
+public:
+  Engine() : _globalTemplate(v8::ObjectTemplate::New())
+  {
+  }
+    
+private:
+  v8::HandleScope _scope;
+  v8::Handle<v8::ObjectTemplate> _globalTemplate;
+  friend class Context;
+  };
+
+class Context
+  {
+public:
+  Context(Engine *e)
+    : _engine(e),
+    _context(v8::Context::New(NULL, _engine->_globalTemplate)),
+    _scope(_context)
+    {
+    }
+
+  ~Context()
+    {
+    // Dispose the persistent context.
+    _context.Dispose();
+    }
+
+  void set(const char* in, const Object& obj)
+    {
+      v8::Handle<v8::String> propName = v8::String::New(in);
+    _context->Global()->Set(propName, obj._object);
+    }
+
+private:
+  Engine* _engine;
+  v8::Persistent<v8::Context> _context;
+  v8::Context::Scope _scope;
+  };
+
+class Script
+  {
+public:
+  Script(const char *data)
+    {
+    // Create a string containing the JavaScript source code.
+    v8::Handle<v8::String> source = v8::String::New(data);
+
+    // Compile the source code.
+    _script = v8::Script::Compile(source);
+    }
+
+  void run()
+    {
+    v8::TryCatch trycatch;
+    // Run the script to get the result.
+    v8::Handle<v8::Value> result = _script->Run();
+
+    if (result.IsEmpty())
+      {  
+      v8::Handle<v8::Value> exception = trycatch.Exception();
+      v8::String::AsciiValue exception_str(exception);
+      printf("Exception: %s\n", *exception_str);
+      // ...
+      }
+
+    // Convert the result to an ASCII string and print it.
+    v8::String::AsciiValue ascii(result);
+    printf("%s\n", *ascii);
+    }
+
+private:
+  v8::Handle<v8::Script> _script;
+  };
+
+
+
+
+
+class SomeClass
+{
+public:
+  virtual ~SomeClass() {}
+
+  virtual int getNonStatic()
+  {
+    return nonStatic;
+  }
+
+  virtual void setNonStatic(int inA)
+  {
+    nonStatic = inA;
+  }
+
+  QVariant getOther(const char *, const QVariant &v)
+  {
+    return nonStatic;
+  }
+
+  virtual void setOther(const int &inA)
+  {
+    nonStatic = inA;
+  }
+
+  static int getA()
+  {
+    return a;
+  }
+
+  static void setA(const int &inA)
+  {
+    a = inA;
+  }
+
+  int nonStatic;
+  static int a;
+};
+int SomeClass::a = 0;
 
 int main(int, char*[])
 {
-  // Create a stack-allocated handle scope.
-  HandleScope handle_scope;
-
-  Handle<ObjectTemplate> global_templ = ObjectTemplate::New();
-
-  Interface someTempl;
-  someTempl.addStaticProperty<int>("a", SomeClass::getA, SomeClass::setA); 
-  someTempl.addProperty<int>("a", &SomeClass::getNonStatic, &SomeClass::setNonStatic); 
-
-  Handle<ObjectTemplate> point_templ = ObjectTemplate::New();
-  point_templ->SetInternalFieldCount(1);
+  Engine engine;
 
   TypeWrapper::wrap<int>();
-  addStaticAccessor(point_templ, "a", qMetaTypeId<int>(), SomeClass::getA, SomeClass::setA);
-  addAccessor(point_templ, "x", qMetaTypeId<int>(), &SomeClass::getNonStatic, &SomeClass::setNonStatic);
+
+  // build the template
+  Interface someTempl;
+  someTempl.addStaticProperty<int>("a", SomeClass::getA, SomeClass::setA); 
+  someTempl.addProperty<int>("a", &SomeClass::getNonStatic, &SomeClass::setNonStatic);
+  someTempl.addPropertyMap(&SomeClass::getOther, &SomeClass::setOther);
 
   // Create a new context.
-  Persistent<Context> context = Context::New(NULL, global_templ);
+  Context c(&engine);
 
   // Enter the created context for compiling and
   // running the hello world script.
-  Context::Scope context_scope(context);
 
-  SomeClass c;
-  Local<Object> obj = point_templ->NewInstance();
-  obj->SetInternalField(0, External::New(&c));
+  SomeClass wrapable;
+  Object obj(&someTempl, &wrapable);
 
-  Handle<String> propName = String::New("someClass");
-  context->Global()->Set(propName, obj);
+  c.set("someClass", obj);
 
-  // Create a string containing the JavaScript source code.
-  Handle<String> source = String::New("someClass.a = 5; someClass.x = 1; 'Hello' + someClass.a.toString() + someClass.x.toString();");
+  Script script("someClass.a = 5; someClass.x = 1; 'Hello' + someClass.a.toString() + someClass.x.toString();");
 
-  // Compile the source code.
-  Handle<Script> script = Script::Compile(source);
-
-  TryCatch trycatch;
-  // Run the script to get the result.
-  Handle<Value> result = script->Run();
-
-  if (result.IsEmpty()) {  
-    Handle<Value> exception = trycatch.Exception();
-    String::AsciiValue exception_str(exception);
-    printf("Exception: %s\n", *exception_str);
-    // ...
-  }
-
-  // Dispose the persistent context.
-  context.Dispose();
-
-  // Convert the result to an ASCII string and print it.
-  String::AsciiValue ascii(result);
-  printf("%s\n", *ascii);
+  script.run();
   return 0;
 }
