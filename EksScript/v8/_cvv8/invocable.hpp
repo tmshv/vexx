71e346ee2d475b4cf958143ef60e817e38fc0224
George Pimm <jorjpimm@googlemail.com>
2012-03-16 13:19:18 +0000

    removed embedded v8 and added a submodule for the qtjsbackend which has some nice extra features

--- EksScript/v8/_cvv8/invocable.hpp
+++
@@ -1,7 +0,0 @@
-#if !defined(CODE_GOOGLE_COM_P_V8_CONVERT_INVOKE_HPP_INCLUDED)
-#define CODE_GOOGLE_COM_P_V8_CONVERT_INVOKE_HPP_INCLUDED 1
-
-#include "XConvertToScript.h"
-#include "XSignature.h"
-#include "detail/invocable_core.hpp"
-#endif /* CODE_GOOGLE_COM_P_V8_CONVERT_INVOKE_HPP_INCLUDED */
